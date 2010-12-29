8b48738089e47398ad7ce4305cdf2ad0214a0894
Juris Galang <jurisgalang@gmail.com>
2010-12-29 14:45:40 -0800

    factored out string parsing into String class

--- lib/howkast/ext/string.rb
+++ lib/howkast/ext/string.rb
@@ -5,4 +5,21 @@ class String
     gsub(/(?:_+|-+)([a-z])/){ $1.upcase }.
     gsub(/(\A|\s)([a-z])/){ $1 + $2.upcase }
   end
+  
+  def parse
+    rfc2822 = "(#{Time::RFC2822_DAY_NAME.join('|')}, )? \d{1,2} #{Time::RFC2822_MONTH_NAME.join('|')} \d{4,4} \d{2,2}:\d{2,2}:\d{2,2} [+-]\d{4,4}"
+    ansi    = "#{Time::RFC2822_DAY_NAME.join('|')} #{Time::RFC2822_MONTH_NAME.join('|')} +\d{1,2} \d{2,2}:\d{2,2}:\d{2,2} [A-Z]{3,3} \d{4,4}"
+    case self
+    when /^[+-]?\d+$/
+      to_i
+    when /^[+-]?\d+\.\d+/
+      to_f
+    when /^(true|false)$/i
+      self =~ /^true$/i ? true : false
+    when /^#{rfc2822}|#{ansi}$/
+      Time.parse(self) rescue self
+    else
+      self
+    end
+  end
 end
--- lib/howkast/model.rb
+++ lib/howkast/model.rb
@@ -29,23 +29,6 @@ class Howkast::Model
   end
   
   def self.parse value
-    rfc2822 = "(#{Time::RFC2822_DAY_NAME.join('|')}, )? \d{1,2} #{Time::RFC2822_MONTH_NAME.join('|')} \d{4,4} \d{2,2}:\d{2,2}:\d{2,2} [+-]\d{4,4}"
-    ansi    = "#{Time::RFC2822_DAY_NAME.join('|')} #{Time::RFC2822_MONTH_NAME.join('|')} +\d{1,2} \d{2,2}:\d{2,2}:\d{2,2} [A-Z]{3,3} \d{4,4}"
-    if value.instance_of? String
-      case value
-      when /^[+-]?\d+$/
-        value.to_i
-      when /^[+-]?\d+\.\d+/
-        value.to_f
-      when /^(true|false)$/i
-        value =~ /^true$/i ? true : false
-      when /^#{rfc2822}|#{ansi}$/
-        Time.parse(value) rescue value
-      else
-        value
-      end
-    else
-      value
-    end
+    value.instance_of?(String) ? value.parse : value
   end
 end
