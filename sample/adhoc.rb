c8c6ae05439d543b26bc0dbc57783728ea87a20b
Juris Galang <jurisgalang@gmail.com>
2011-01-03 21:06:38 -0800

    Added adhoc.rb

--- sample/adhoc.rb
+++ sample/adhoc.rb
@@ -9,14 +9,21 @@ end
 
 Howkast::configure YAML.load File.read('.howkast')
 howcast = Howkast::API.new
-search  = howcast.search :query => 'jujitus'
-puts search.videos.count
+video   = howcast.video :id => 3907
+puts video.category_hierarchy.first.name
 
-search  = howcast.search :query => 'jujitsu'
-puts search.videos.count
+video   = howcast.video :id => 5072
+puts video.category_hierarchy
+puts video.category_hierarchy.first.name
 
-search  = howcast.search :query => ''
-puts search.videos.count
-search.videos.each do |video|
-  puts video.title
-end
+#search  = howcast.search :query => 'jujitus'
+#puts search.videos.count
+#
+#search  = howcast.search :query => 'jujitsu'
+#puts search.videos.count
+#
+#search  = howcast.search :query => ''
+#puts search.videos.count
+#search.videos.each do |video|
+#  puts video.title
+#end
