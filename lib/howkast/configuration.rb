2114426213a1ac1780ed80093e6f452836d45eee
Juris Galang <jurisgalang@gmail.com>
2011-01-05 17:25:35 -0800

    got rid of deadcode

--- lib/howkast/configuration.rb
+++ lib/howkast/configuration.rb
@@ -4,14 +4,14 @@ module Howkast
   class Configuration
     include Singleton
     attr_accessor :api_key
-    attr_accessor :cache_timeout
-    attr_accessor :cache_location
-    attr_accessor :cache_store
+    #attr_accessor :cache_timeout
+    #attr_accessor :cache_location
+    #attr_accessor :cache_store
     
     def initialize
-      @cache_timeout  = 600
-      @cache_location = '/tmp/howkast/cache'
-      @cache_store    = 'file'
+      #@cache_timeout  = 600
+      #@cache_location = '/tmp/howkast/cache'
+      #@cache_store    = 'file'
     end
   end
 end
